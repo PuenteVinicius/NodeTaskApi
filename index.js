0cb72c001fab5f2d31b1748540cfda94442ae0a8
VInicius <viniciuspuente_2@hotmail.com>
2017-01-24 17:49:12 -0200

    File auth.js added.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 consign()
     .include("libs/config.js")
     .then("db.js")
+    .then("auth.js")
     .then("libs/middlewares.js")
     .then("routes")
     .then("libs/boot.js")
