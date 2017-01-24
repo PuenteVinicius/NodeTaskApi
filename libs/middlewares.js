dff500417e752deed344692c4ca7aab04775385b
VInicius <viniciuspuente_2@hotmail.com>
2017-01-24 15:11:32 -0200

    CRUD added at routes/tasks.js

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,15 @@
+import bodyParser from "body-parser";
 module.exports = app => {
-  app.set("port", 3000);
-  app.set("json spaces", 4);
+    app.set("port", 3000);
+    app.set("json spaces", 4);
 };
+module.exports = app => {
+    app.set("port", 3000);
+    app.set("json spaces", 4);
+    app.use(bodyParser.json());
+    app.use((req, res, next) => {
+        delete req.body.id;
+        next();
+    });
+};
+app.use(bodyParser.json())
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,70 @@
 module.exports = app => {
     const Tasks = app.db.models.Tasks;
-    app.get("/tasks", (req, res) => {
-        Tasks.findAll({}).then(tasks => {
-            res.json({
-                tasks: tasks
-            });
+    app.route("/tasks")
+        .get((req, res) => {
+            Tasks.findAll({})
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+            // "/tasks": Lista tarefas
+        })
+        .post((req, res) => {
+            Tasks.create(req.body)
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+            // "/tasks": Cadastra uma nova tarefa
         });
-    });
+    app.route("/tasks/:id")
+        .all((req, res) => {
+            delete req.body.id;
+            next();
+            // Middleware de pré-execução das rotas
+        })
+        .get((req, res) => {
+            Tasks.findOne({
+                    where: req.params
+                })
+                .then(result => {
+                    if (result) {
+                        res.json(result);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                })
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+        })
+        .put((req, res) => {
+            Tasks.update(req.body, {
+                    where: req.params
+                })
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                }); // "/tasks/1": Atualiza uma tarefa
+        })
+        .delete((req, res) => {
+            Tasks.destroy({
+                    where: req.params
+                })
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                });
+            // "/tasks/1": Exclui uma tarefa
+        });
 };
