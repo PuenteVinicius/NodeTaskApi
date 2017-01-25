991c303cb1f85ce52687ede8541c2747920a6b85
VInicius <viniciuspuente_2@hotmail.com>
2017-01-25 17:49:05 -0200

    Tasks and Users modified.

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,13 +4,16 @@ module.exports = app => {
     app.route("/tasks")
     .all(app.auth.authenticate())
     .get((req, res) => {
-      Tasks.findAll({})
+      Tasks.findAll({
+        where: { user_id: req.user.id }
+      })
         .then(result => res.json(result))
         .catch(error => {
           res.status(412).json({msg: error.message});
         });
     })
     .post((req, res) => {
+      req.body.user_id = req.user.id;
       Tasks.create(req.body)
         .then(result => res.json(result))
         .catch(error => {
@@ -20,7 +23,10 @@ module.exports = app => {
     app.route("/tasks/:id")
     .all(app.auth.authenticate())
     .get((req, res) => {
-      Tasks.findOne({where: req.params})
+      Tasks.findOne({where:{
+        id:req.params.id,
+        user_id: req.user.id
+      }})
         .then(result => {
           if(result){
             res.json(result);
@@ -33,14 +39,20 @@ module.exports = app => {
         });
     })
     .put((req, res) => {
-      Tasks.update(req.body, {where: req.params})
+      Tasks.update(req.body, {where: {
+        id: req.params.id,
+        user_id: req.user.id
+      }})
         .then(result => res.sendStatus(204))
         .catch(error => {
           res.status(412).json({msg: error.message});
         });
     })
     .delete((req, res) => {
-      Tasks.destroy({where: req.params})
+      Tasks.destroy({where: {
+        id: req.params.id,
+        user_id: req.user.id
+      }})
         .then(result => res.sendStatus(204))
         .catch(error => {
           res.status(412).json({msg: error.message});
