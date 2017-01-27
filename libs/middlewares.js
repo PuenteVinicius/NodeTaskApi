d8577f2aecdfd840f4338c4f66194f5f129ddbb4
VInicius <viniciuspuente_2@hotmail.com>
2017-01-27 13:50:37 -0200

    File logger.js added.

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -7,6 +7,11 @@ module.exports = app => {
 module.exports = app => {
     app.set("port", 3000);
     app.set("json spaces", 4);
+    app.use(cors({
+      origin: ["http://localhost:3001"],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"]
+    }));
     app.use(bodyParser.json());
     app.use(app.auth.initialize());
     app.use((req, res, next) => {
